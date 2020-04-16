b3d51f622f67823874bfb772c38008dba9fc8825
gpforad0 <gpforad0@gmail.com>
2020-04-15 23:29:36 -0400

    my first commit

---
+++ src/upload.py
@@ -0,0 +1,13 @@
+import gitsrc
+
+gitsrc.info()
+
+user = 'gpforad0'
+
+email = 'gpforad0@gmail.com'
+
+scriptname = 'upload.py'
+
+giturl = 'https://github.com/gpforad0/test22.git'
+
+gitsrc.push(user, email, scriptname, giturl)
